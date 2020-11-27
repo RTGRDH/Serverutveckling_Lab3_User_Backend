5ffed3529c68dcaa9d793a302bc744647780c686
Ernst Reutergårdh <kedre@kth.se>
2020-11-27 15:40:24 +0100

    -Init Commit

---
+++ src/main/java/User_Backend/demo/ServerutvecklingLab3UserBackendApplication.java
@@ -0,0 +1,13 @@
+package User_Backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerutvecklingLab3UserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerutvecklingLab3UserBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/User_Backend/demo/ServerutvecklingLab3UserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package User_Backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerutvecklingLab3UserBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
